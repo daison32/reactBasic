eda9021f6fa8bcb05e12231ed82fb18b6e9a334a
Karelia <sunagimogushi0406@gmail.com>
2022-07-10 23:27:46 +0900

    axios使わない。 setTasks(data.tasks);

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,32 +1,28 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 const Fetch = () => {
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const [body, settasks] = useState([])
-    
-    useEffect(() => {
-        fetch('https://gorensyu.herokuapp.com/tasks', {method: 'GET'})
-        .then(res => res.json())
-        .then(data => {
-            settasks(data)
-        })
-    },[])
-
-    return (
-        <div>
-            <ul>
-
-
-        {
-          body.tasks.map(post => 
-            <li key={post.id}>{post.content}</li>
-            )
-        }
-        
-            </ul>
-
-        </div>
-    )
-}
+  useEffect(() => {
+    fetch("https://gorensyu.herokuapp.com/tasks", { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setTasks(data.tasks);
+        setLoading(false);
+      });
+  }, []);
+  if (loading) return <p>loading ...</p>;
+  return (
+    <div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Fetch;
