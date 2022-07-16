94b1dc794299eb20ad646eb599525e6d249ce7be
Karelia <sunagimogushi0406@gmail.com>
2022-07-16 09:18:08 +0900

    React reviewed

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,43 +1,36 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState } from 'react'
 
 const Post = () => {
   const [formData, setFormData] = useState({})
-
 
   // リアルタイムで更新
   const handleChange = (event) => {
     setFormData({ content: event.target.value })
   }
 
-  const eventHandler = (event) => {
+  const eventHandler = () => {
     fetch('http://localhost:8080/tasks', {
       method: 'POST',
       handlers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        content:formData
-      }),
+      body: JSON.stringify(formData),
     })
       .then((res) => res.json())
       .then((result) => {
         console.log(result)
         setFormData('')
       })
-      .catch((err) => console.log('error'))
+      .catch((err) => console.log('error detail', err))
   }
 
-
-return (
-  <div className="App">
-    <form>
-      <input name="name" value={formData.name || ''} onChange={handleChange} />
+  return (
+    <div className='App'>
+      <input value={formData.name} onChange={handleChange} />
       <button onClick={eventHandler}>click</button>
-    </form>
-    
-  </div>
-)
+    </div>
+  )
 }
 
 export default Post
+
