3b06b965020e8e1f69af367a09307d3291d1cadc
Karelia <sunagimogushi0406@gmail.com>
2022-07-20 17:39:51 +0900

    post をaxios

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import axios from "axios";
+
+const baseURL = "https://gorensyu.herokuapp.com/tasks"
 
 const Post = () => {
   const [formData, setFormData] = useState({})
@@ -9,21 +12,13 @@ const Post = () => {
   }
 
   const eventHandler = () => {
-    fetch('https://gorensyu.herokuapp.com/tasks', {
-      method: 'POST',
-      handlers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((res) => res.json())
+    console.log(formData)
+    axios
+      .post(baseURL, formData)
+      .then((res) => console.log(res))
 
       .then((result) => {
-        console.log(result)
-        console.log(formData)
         setFormData("")
-        console.log(formData)
-
       })
       .catch((err) => console.log('error detail', err))
   }
@@ -38,6 +33,5 @@ const Post = () => {
 }
 
 export default Post
-
 
 
